1ff57d9086e6aad8834ee97cbb7bb094b7657197
hukuryo <ryohei.1998.05.09@gmail.com>
2024-10-01 22:23:32 +0900

    Server Actionsを実装

--- app/(shop)/account/page.tsx
+++ app/(shop)/account/page.tsx
@@ -1,5 +1,25 @@
-import React from "react";
-
 export default function page() {
-  return <div>shop page</div>;
+  async function onAction(formData: FormData) {
+    "use server";
+
+    const name = formData.get("name");
+    const email = formData.get("email");
+    console.log("name : " + name);
+    console.log("email : " + email);
+  }
+  return (
+    <main>
+      {/*Server Actions*/}
+      <form action={onAction}>
+        <label htmlFor="name">name: </label>
+        <input id="name" name="name" />
+        <br />
+        <label htmlFor="email">email: </label>
+        <input id="email" name="email" />
+        <br />
+        <button type="submit">Server Action</button>
+        <br />
+      </form>
+    </main>
+  );
 }
